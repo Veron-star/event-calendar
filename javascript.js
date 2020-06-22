9f01107331015e8ad2852f5b8e235f0dee24a840
Veron-star <64481387+Veron-star@users.noreply.github.com>
2020-06-22 14:36:24 +0800

    uploaded README and Sreenshot

--- javascript.js
+++ javascript.js
@@ -1,18 +1,31 @@
-$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
+
+  $(function() {
+    $('input[name="datefilter"]').daterangepicker({
+        autoUpdateInput: false,
+        locale: {
+            cancelLabel: 'Clear'
+        }
+    });
+    $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+        $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+    });
+    $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+        $(this).val('');
+    });
+  });
+
+$(document).ready(function () {
     //display current day & time.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-    //Assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
-        //get nearby values.
-        console.log(this);
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        //set items in local storage.
         localStorage.setItem(time, text);
     })
-    //load any saved data from LocalStorage - do this for each hour created.
+    
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -22,18 +35,16 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
 
 
     function hourTracker() {
         //get current number of hours.
         var currentHour = moment().hour();
 
-        // loop over time blocks
         $(".time-block").each(function () {
             var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-            console.log( blockHour, currentHour)
-
-            //check if we've moved past this time
+            
             if (blockHour < currentHour) {
                 $(this).addClass("past");
                 $(this).removeClass("future");
